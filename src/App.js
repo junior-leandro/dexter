41efeca88bb07b2f149ee2d86e9e6c00a18bbafa
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-04-09 10:58:34 -0300

    feat: component season

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Season from './components/Season/Season';
 
 function App() {
   return (
     <>
     <Header/>
     <div className="App">
-      
-      
+      <Season season="1"/>
+      <Season season="2"/>
+      <Season season="3"/>
+      <Season season="4"/>
+      <Season season="5"/>
+      <Season season="6"/>
+      <Season season="7"/>
+      <Season season="8"/>
     </div>
     </>
   );
---
+++ src/components/Season/Season.js
@@ -0,0 +1,9 @@
+import './season.css';
+
+export default function Season(props) {
+    return (
+        <div className='season-content'>
+         <h1>SEASON {props.season} WORKING</h1>
+        </div>
+    );
+}
