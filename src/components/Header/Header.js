15996fa260ac46d624b63b2a7ed063f068827c81
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-04-13 17:16:05 -0300

    feat: consumindo json

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,14 +12,14 @@ export default function Header() {
 
             <div className="header-area-2">
                 <ul className='header-list'>
-                    <li><a href="#" target='_blank'>SEASON 01</a></li>
-                    <li><a href="#" target='_blank'>SEASON 02</a></li>
-                    <li><a href="#" target='_blank'>SEASON 03</a></li>
-                    <li><a href="#" target='_blank'>SEASON 04</a></li>
-                    <li><a href="#" target='_blank'>SEASON 05</a></li>
-                    <li><a href="#" target='_blank'>SEASON 06</a></li>
-                    <li><a href="#" target='_blank'>SEASON 07</a></li>
-                    <li><a href="#" target='_blank'>SEASON 08</a></li>
+                    <li><a href="#">SEASON 01</a></li>
+                    <li><a href="#">SEASON 02</a></li>
+                    <li><a href="#">SEASON 03</a></li>
+                    <li><a href="#">SEASON 04</a></li>
+                    <li><a href="#">SEASON 05</a></li>
+                    <li><a href="#">SEASON 06</a></li>
+                    <li><a href="#">SEASON 07</a></li>
+                    <li><a href="#">SEASON 08</a></li>
                 </ul>
 
             </div>
--- src/components/Season/Season.js
+++ src/components/Season/Season.js
@@ -1,13 +1,25 @@
+import React from 'react';
+import data from "./season.json"
 import './season.css';
 
-export default function Season(props) {
+
+
+function Season(props) {
+
+    let currentSeason = props.season
+    let currentId = currentSeason - 1;
+
     return (
         <div className='season-content'>
-         <h1>SEASON {props.season} WORKING</h1>
+         <h1>SEASON {currentSeason} WORKING</h1>
+
          <div>
-            <h2>{props.season} st Summary</h2>
+            <h2>Synopsis</h2>
+            <p>{data[currentId].synopsis}</p>
+            <h3>Year: {data[currentId].year} </h3>
          </div>
         </div>
     );
 }
+export default Season;
 
