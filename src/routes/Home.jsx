2a9c6a3b794885792436f29e6acfd12183352ab6
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-04-20 12:52:04 -0300

    Home update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,12 @@
 import './home.css'
-import Header from '../components/Header/Header';
 import videoBG from '../assets/video/video.mp4';
 
 function Home() {
 
-    let currentSeason = 1
-
     return (
         <div className="home">
             <div className="overlay"></div>
-            <video src={videoBG} autoPlay loop muted />
+            <video src={videoBG} autoPlay loop muted/>
             <div className="content">
                 <h1>Welcome to Dexter's Life</h1>
                 <a href="/season">Explore</a>
